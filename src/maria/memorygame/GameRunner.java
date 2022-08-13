8852b572a0fce8d36eb8590a2c1a0b1d4651615c
Maria <maria.zelek@interia.com>
2022-08-13 14:34:02 +0200

    Reading DifficultyLevel

--- src/maria/memorygame/GameRunner.java
+++ src/maria/memorygame/GameRunner.java
@@ -1,9 +1,26 @@
 package maria.memorygame;
 
-import maria.memorygame.data.WordsProvider;
+import maria.memorygame.model.DifficultyLevel;
+
+import java.util.Scanner;
 
 public class GameRunner {
     public static void main(String[] args) {
-        System.out.println(new WordsProvider().getWords());
+            DifficultyLevel level = readDifficultyLevel();
+    }
+
+    public static DifficultyLevel readDifficultyLevel() {
+        DifficultyLevel level = null;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Select difficulty level.");
+        while (level == null) {
+            System.out.print("Type 'easy' or 'hard':");
+            String line = in.nextLine();
+            level = DifficultyLevel.byDisplayName(line);
+            if (level == null) {
+                System.out.println("Unknown difficulty level:" + line);
+            }
+        }
+        return level;
     }
 }
---
+++ src/maria/memorygame/model/DifficultyLevel.java
@@ -0,0 +1,24 @@
+package maria.memorygame.model;
+
+public enum DifficultyLevel {
+    EASY("easy",4,10),
+    HARD("hard",8,15);
+    private final String displayName;
+    private final int numberOfWords;
+    private final int numberOfChances;
+
+    DifficultyLevel(String displayName, int numberOfWords, int numberOfChances) {
+        this.displayName = displayName;
+        this.numberOfWords = numberOfWords;
+        this.numberOfChances = numberOfChances;
+    }
+
+    public static DifficultyLevel byDisplayName(String displayName){
+        for (DifficultyLevel value : DifficultyLevel.values()) {
+            if(value.displayName.equals(displayName)){
+                return value;
+            }
+        }
+        return null;
+    }
+}
